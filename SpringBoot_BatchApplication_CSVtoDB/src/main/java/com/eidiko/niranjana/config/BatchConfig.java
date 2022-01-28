bf21730eff2b9fc3422f5227c45c50fac24a1a0a
Niranjana <NiranjanaDEV@gmail.com>
2022-01-28 04:43:46 -0800

    BatchProcessing1

---
+++ SpringBoot_BatchApplication_CSVtoDB/src/main/java/com/eidiko/niranjana/config/BatchConfig.java
@@ -0,0 +1,134 @@
+package com.eidiko.niranjana.config;
+import java.util.Date;
+import javax.sql.DataSource;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import com.eidiko.niranjana.entity.Product;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+	//1. reader
+	@Bean
+	public ItemReader<Product> reader() {
+		FlatFileItemReader<Product> reader = new FlatFileItemReader<>();
+		//1. Load file name + location
+		reader.setResource(new ClassPathResource("products.csv"));
+		//2. Read Data Line by Line
+		reader.setLineMapper(new DefaultLineMapper<Product>() {{ 
+			//3. tokenize data, provide names
+			setLineTokenizer(new DelimitedLineTokenizer() {{
+				setDelimiter(DELIMITER_COMMA);
+				setNames("prodId","prodCode","prodCost");
+			}});
+			
+			//4. convert to object
+			setFieldSetMapper(new BeanWrapperFieldSetMapper<>() {{
+				setTargetType(Product.class);
+			}});
+		}});
+		return reader;
+	}
+	//2. processor
+	/*@Bean
+	public ItemProcessor<Product, Product> procesor() {
+		return new ProductProcessor();
+	}*/
+	@Bean
+	public ItemProcessor<Product, Product> procesor() {
+		return (item) -> {
+			double cost = item.getProdCost();
+			
+			item.setProdGst(cost*12/100.0);
+			item.setProdDisc(cost*20/100.0);
+			
+			return item;
+		};
+	}
+	
+	@Autowired
+	private DataSource dataSource;
+	//3. writer
+	@Bean
+	public ItemWriter<Product> writer() {
+		JdbcBatchItemWriter<Product> writer = new JdbcBatchItemWriter<>();
+		writer.setSql("INSERT INTO PRODUCTS  (PID, PCODE , PCOST , PGST , PDISC ) VALUES (:prodId, :prodCode, :prodCost, :prodGst, :prodDisc)");
+		writer.setDataSource(dataSource);
+		writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
+		return writer;
+	}
+	
+	//4. listener
+	/*@Bean
+	public JobExecutionListener listener() {
+		return new MyJobListener();
+	}*/
+	
+	@Bean
+	public JobExecutionListener listener() {
+		return new JobExecutionListener() {
+
+			public void beforeJob(JobExecution je) {
+				System.out.println("Job starts on => " + je.getStatus());
+				System.out.println("job starts date is => " + new Date());
+			}
+
+			public void afterJob(JobExecution je) {
+				System.out.println("Job ends on => " + je.getStatus());
+				System.out.println("Jobs ends date => " + new Date());
+			}
+		};
+		
+	}
+	
+	//5. StepBuilderFactory
+	@Autowired
+	private StepBuilderFactory sf;
+	
+	//6. Step
+	@Bean
+	public Step stepA() {
+		return sf.get("stepA")
+				.<Product,Product>chunk(3)
+				.reader(reader())
+				.processor(procesor())
+				.writer(writer())
+				.build();
+	}
+	
+	//7. JobBuilderFactory
+	@Autowired
+	private JobBuilderFactory jf;
+	
+	//8. Job
+	@Bean
+	public Job jobA() {
+		return jf.get("jobA")
+				.incrementer(new RunIdIncrementer())
+				.listener(listener())
+				.start(stepA())
+				.build()
+				;
+	}
+}
---
+++ SpringBoot_BatchApplication_CSVtoDB/src/main/java/com/eidiko/niranjana/entity/Product.java
@@ -0,0 +1,16 @@
+package com.eidiko.niranjana.entity;
+
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+	private Integer prodId;
+	private String prodCode;
+	private Double prodCost;
+	
+	private Double prodGst;
+	private Double prodDisc;
+}
+//create table products(pid int,pcode varchar(20), pcost double, pdisc double,pgst double);
