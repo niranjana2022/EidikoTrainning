059e8b21876e1905922d936328275bc5004b2d13
Niranjana <NiranjanaDEV@gmail.com>
2022-01-28 05:00:00 -0800

    Modified

--- SpBootWeb_SS_LPOC/src/main/java/com/eidiko/niranjana/ssl/SecurityConfig.java
+++
@@ -1,9 +0,0 @@
-/*
- * package com.eidiko.niranjana.ssl;
- * 
- * @EnableWebSecurity public class SecurityConfig extends
- * WebSecurityConfigurerAdapter {
- * 
- * @Override protected void configure(HttpSecurity http) throws Exception {
- * http.authorizeRequests() .antMatchers("/**") .permitAll(); } }
- */
