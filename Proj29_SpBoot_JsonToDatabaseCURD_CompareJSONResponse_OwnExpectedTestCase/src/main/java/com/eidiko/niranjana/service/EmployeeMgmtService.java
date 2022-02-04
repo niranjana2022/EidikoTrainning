c1f2ec19a9c65903fce781637aa1f6a3e01feff0
Niranjana <NiranjanaDEV@gmail.com>
2022-02-04 07:57:16 -0500

    proj29_04/02/2022

--- Proj29_SpBoot_JsonToDatabaseCURD_CompareJSONResponse_OwnExpectedTestCase/src/main/java/com/eidiko/niranjana/service/EmployeeMgmtService.java
+++
@@ -1,8 +0,0 @@
-package com.eidiko.niranjana.service;
-public interface EmployeeMgmtService {
-
-		public int fetchEmpsCount();
-		public String registerEmp(Integer eId,String eName, String eAddrs);
-		public String updateEmpService(Integer eId,String eName, String eAddrs);
-		public String deleteEmpService(Integer eId);
-}
